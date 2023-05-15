21832efeb5b3c036b5a12dcadcc297e263c54d10
Felipe Lussi <felipe-p-l@hotmail.com>
2023-05-15 17:40:52 -0300

    Finalizei a aula 3

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
 
+
+
+//ROTA
+// method HTTP - CRUD (CREATE, READ, UPTDATE, DELETE)
+  //GET - pega uma info
+  //POST - Cria uma info
+  //PUT - altera toda a infos
+  //PATH - altera parte da info
+  //DELETE - apaga uma info
+
+// Name - Um identificador da rota
+
+// Function (calback) - Responsável por enxecutar algum comando
+
 app.get('/home',  (req, res) => {
-  res.send('Hello World')
+
+  const soma =  1+1;
+
+  res.json(soma);
+
 })
 
-app.listen(3000)
+
+
+app.listen(3000);
