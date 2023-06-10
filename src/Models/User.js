d7730686a5a35b53a879f5520e6706e132f449e3
Felipe Lussi <felipe-p-l@hotmail.com>
2023-06-10 11:04:50 -0300

    bcrypt added - password is stored encrypted

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, select:false },
   avatar: { type: String, required: true },
   background: { type: String, required: true },
 });
 
-const user = mongoose.model("User", userSchema)
+userSchema.pre('save', async function (next){
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+} )
+
+const user = mongoose.model("User", userSchema);
 
 export default user;
